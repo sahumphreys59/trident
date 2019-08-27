9ab47e2f4df9409a5ed6bfe7a100d496a027a813
sahumphreys59 <sarah.humphreys59@gmail.com>
2019-08-27 15:09:08 -0700

    initial commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,24 @@
+class PagesController < ApplicationController
+	
+	def home
+	end	
+
+	def software
+	end
+	
+	def services
+	end
+
+	def support
+	end
+
+	def news
+	end
+
+	def aboutUs
+	end
+
+	def contact
+	end	
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root'pages#home'
+	get '/software' => 'pages#software'
+	get '/services' => 'pages#services'
+	get '/support' => 'pages#support'
+	get '/news' => 'pages#news'
+	get '/aboutUs' => 'pages#aboutUs'
+	get '/contact' => 'pages#contact'
+
+
+end
