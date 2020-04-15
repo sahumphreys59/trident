97ebf1b6df27ec35ba8217c78cc69853c53f3e9c
sahumphreys59 <sarah.humphreys59@gmail.com>
2020-04-15 11:11:20 -0700

    heroku authentication

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,7 +98,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
-    port:                 25,
+    port:                 587,
     domain:               'gmail.com',
     user_name:            ENV["GMAIL_EMAIL"],
     password:             ENV["GMAIL_PASSWORD"],
