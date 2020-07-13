3eb31287b6cfe42343f7f0ab91515c4ed02419be
sahumphreys59 <sarah.humphreys59@gmail.com>
2020-07-13 14:22:17 -0700

    saving checkpoint

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,13 +96,13 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors= true
   config.action_mailer.delivery_method = :smtp
-  
+
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
     domain:               'gmail.com',
     user_name:            ENV["GMAIL_EMAIL"],
     password:             ENV["GMAIL_PASSWORD"],
-    authentication:       'plain',
+    authentication:       :plain,
     enable_starttls_auto: true  }
 end
